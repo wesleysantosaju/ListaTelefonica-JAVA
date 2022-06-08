c2eae21fce154221af0c6b06eeda5a9787eaf7eb
Wesley Santos <wvsantos15@gmail.com>
2022-06-08 10:04:26 -0300

    Initial
    Initial

---
+++ Back-end/src/main/java/com/ws/listatelefoinica/ListatelefonicaApplication.java
@@ -0,0 +1,12 @@
+package com.ws.listatelefoinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListatelefonicaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ListatelefonicaApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/ws/listatelefoinica/ListatelefonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.listatelefoinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListatelefonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
