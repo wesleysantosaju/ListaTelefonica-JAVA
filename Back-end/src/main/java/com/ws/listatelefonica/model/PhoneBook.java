42bd2a65b485b7b72926dfb1abe044a9d7fe56f0
Wesley Santos <wvsantos15@gmail.com>
2022-06-08 10:48:57 -0300

    Creating constructors and getters and setters
    Creating constructors and getters and setters

--- Back-end/src/main/java/com/ws/listatelefonica/model/PhoneBook.java
+++ Back-end/src/main/java/com/ws/listatelefonica/model/PhoneBook.java
@@ -21,4 +21,75 @@ public class PhoneBook {
     private String comments;
     private Operators operators;
 
+    //region Bloco - Contrutores
+    public PhoneBook(){}
+
+    public PhoneBook(String name, String address, String phone, String email, String comments, Operators operators) {
+        super();
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.comments = comments;
+        this.operators = operators;
+    }
+    //endregion
+
+    //region Bloco - Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Operators getOperators() {
+        return operators;
+    }
+
+    public void setOperators(Operators operators) {
+        this.operators = operators;
+    }
+    //endregion
 }
